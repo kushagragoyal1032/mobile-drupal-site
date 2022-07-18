999b306075e676e08b2e3561d4cc06a7ae3516c3
kushagra goyal <kushagragoyal1032@gmail.com>
2022-07-18 10:54:36 +0530

    change js file

--- web/themes/custom/final/js/home.js
+++ web/themes/custom/final/js/home.js
@@ -1,7 +1,7 @@
 
 
 const rows = document.querySelectorAll('.cont-container div#block-final-content div div.views-row');
-// console.log(rows);
+console.log(rows);
 for (const row of rows) {
   row.classList.add('col-lg-3');
   row.classList.add('col-sm-6');
@@ -11,21 +11,25 @@ for (const row of rows) {
   row.classList.add('pt-2');
   row.classList.add('p-sm-3');
 }
-const is_title_exist = document.querySelector('#block-final-page-title');
-const data = document.querySelectorAll('.contextual-region');
+// const is_title_exist = document.querySelector('#block-final-page-title');
+// const data = document.querySelectorAll('.contextual-region');
 
-if(is_title_exist)
-{
-  console.log("present");
-  data[4].classList.add('row');
-}
-else
-{
-  console.log("not present");
-  data[3].classList.add('row');
-}
+// if(is_title_exist)
+// {
+//   console.log("present");  // it title present then we have to add row in "data[4]" class (use when we click on texo. of any products)
+//   data[4].classList.add('row');
+// }
+// else
+// {
+//   console.log("not present");
+//   data[3].classList.add('row');
+// }
 
-console.log(data);  
+// console.log(data);  
+
+const row_contain = document.querySelector('.views-element-container div');
+row_contain.classList.add('row');
+console.log(row_contain);
 
 
 const btn_values = document.querySelectorAll('.cont-container .views-row .card-title a');
